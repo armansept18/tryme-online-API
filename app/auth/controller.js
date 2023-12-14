cb1e0a2a973e827b7fd0a0ea1a509cfea9b583f9
Arman Septian Pratama <armansept18@gmail.com>
2023-12-14 21:22:40 +0700

    feat: login

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,5 +1,8 @@
 const User = require("../user/model");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 
 const register = async (req, res, next) => {
   try {
@@ -35,6 +38,48 @@ const register = async (req, res, next) => {
   }
 };
 
+const localStrategy = async (email, password, done) => {
+  try {
+    const user = await User.findOne({ email }).select(
+      "-__v -createdAt -updatedAt -cart_items -token"
+    );
+    if (!user) {
+      return done();
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      ({ password, ...userWithoutPassword } = user.toJSON());
+      return done(null, userWithoutPassword);
+    }
+  } catch (err) {
+    done(err, null);
+  }
+  done();
+};
+
+const login = async (req, res, next) => {
+  passport.authenticate("local", async function (err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res
+        .status(404)
+        .json({ error: 1, message: "Email or Password Incorrect!" });
+    }
+    const signIn = jwt.sign(user, config.secretkey);
+
+    await User.findByIdAndUpdate(user._id, { $push: { token: signIn } });
+
+    res.status(200).json({
+      message: "Login Successfully",
+      user,
+      token: signIn,
+    });
+  });
+};
+
 module.exports = {
   register,
+  login,
+  localStrategy,
 };
