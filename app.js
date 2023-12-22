ba7d95132a8b44c690d408c2b097b5584e0e5d11
Arman Septian Pratama <armansept18@gmail.com>
2023-12-22 07:30:27 +0700

    fix: app.js view

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const invoiceRoute = require("./src/routes/invoice");
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/src/views"));
 app.set("view engine", "jade");
 
 app.use(cors());
